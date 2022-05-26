5603da8d81940c18f199c4d56e749fb6cb24d41a
MauriBian <mauricioosvaldobianchi@gmail.com>
2022-05-26 09:10:48 -0300

    Label in question model

--- models/question.js
+++ models/question.js
@@ -3,6 +3,7 @@ const { mongoose } = require('../db/mongoose')
 const question = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId, 
     text: String,
+    label: String,
     questionType: String,
     responseType: String,
     path: String,
